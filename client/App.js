332657cb34a292e06977fd4250025fe2b7329659
Ebube <ugbore2@gmail.com>
2024-11-07 10:19:11 +0100

    changes

--- client/App.js
+++ client/App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Changes you make will automatically reload.</Text>
+      <Text>i changed something</Text>
       <StatusBar style="auto" />
     </View>
   );
